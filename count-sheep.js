f972b918ce4c8685ed0fc11be8f7f3a12c7df7a4
Wiebke Hagenauer <wiebosaurus@hotmail.com>
2023-03-29 17:12:08 +0200

    update - 2nd solution

--- count-sheep.js
+++ count-sheep.js
@@ -1,7 +1,8 @@
 // If you can't sleep, just count sheep
-// Given a non-negative integer, 3 for example,
-// return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
-// Input will always be valid, i.e. no negative integers.
+
+/* Given a non-negative integer, 3 for example,
+ return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
+ Input will always be valid, i.e. no negative integers.*/
 
 var countSheep = function (num) {
   let sheepCount = "";
@@ -10,5 +11,14 @@ var countSheep = function (num) {
   }
   return sheepCount;
 };
-
 countSheep(3);
+
+var countSheep2 = function (number) {
+  let sheepCount2 = [];
+  for (let i = 1; i <= number; i++) {
+    sheepCount2.push(`${i} sheep...`);
+  }
+  return sheepCount2.join();
+};
+
+countSheep2(3);
